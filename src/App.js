0bde03654e8fd82be8ecd443228841f0cc7418b9
calebdeji <calebdeji06@gmail.com>
2020-09-27 16:18:40 +0100

    refactor: change initial markups

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
+import Layout from './containers/Layout/Layout';
+import Transactions from './containers/Transactions/Transactions';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Switch>{/* <Route path='/transactions' component={Transactions} /> */}</Switch>
+      </Layout>
+    </Router>
   );
-}
+};
 
 export default App;
